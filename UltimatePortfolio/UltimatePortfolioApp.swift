86b985d471b641e6a47862f1a13b463e4b8a658b
Gus Eulalio <guga.emc@gmail.com>
2021-03-07 18:07:21 +1100

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Gustavo E M Cabral on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
