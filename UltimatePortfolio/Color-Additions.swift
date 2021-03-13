dae3c6f57f7b4e852e7745a960df5d7b6fab7937
Gus Eulalio <guga.emc@gmail.com>
2021-03-13 22:17:27 +1100

    Building the Home view

---
+++ UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,14 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Gustavo E M Cabral on 13/3/21.
+//
+
+import SwiftUI
+
+extension Color
+{
+	static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+	static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
